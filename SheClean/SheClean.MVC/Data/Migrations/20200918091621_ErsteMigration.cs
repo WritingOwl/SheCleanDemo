c6070b9981d16aca950fb5f4c1b305ffaa444967
b.wehner <b.wehner@synios.de>
2020-09-18 11:32:19 +0200

    Seite erstellt

---
+++ SheClean/SheClean.MVC/Data/Migrations/20200918091621_ErsteMigration.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace SheClean.MVC.Data.Migrations
+{
+    public partial class ErsteMigration : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+
+        }
+    }
+}
