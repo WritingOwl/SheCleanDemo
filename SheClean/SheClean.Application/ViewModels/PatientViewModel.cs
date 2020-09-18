dc115d7132d949674ed0d1593130f1ec6d69a179
b.wehner <b.wehner@synios.de>
2020-09-18 13:35:29 +0200

    Commit Service, Repository, Interfaces und PatientViewModel

---
+++ SheClean/SheClean.Application/ViewModels/PatientViewModel.cs
@@ -0,0 +1,12 @@
+﻿using SheClean.Domain.Models;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace SheClean.Application.ViewModels
+{
+    public class PatientViewModel
+    {
+        public IEnumerable<Patient> Patients { get; set; }
+    }
+}
---
+++ SheClean/SheClean.Domain/Interfaces/IPatientRepository.cs
@@ -0,0 +1,12 @@
+﻿using SheClean.Domain.Models;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace SheClean.Domain.Interfaces
+{
+    public interface IPatientRepository
+    {
+       IEnumerable<Patient> GetPatient(); 
+    }
+}
