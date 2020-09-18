d0b3b8e447dace71f38d975057190102c4599d84
b.wehner <b.wehner@synios.de>
2020-09-18 11:43:14 +0200

    Patient Domain Model eingefügt

---
+++ SheClean/SheClean.Domain/Models/Patient.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace SheClean.Domain.Models
+{
+    class Patient
+    {
+        public int Id { get; set; }
+        public string Vorname { get; set; }
+        public string Nachname { get; set; }
+        public string PatentNr { get; set; }
+        public string ImageUrl { get; set; }
+    }
+}
